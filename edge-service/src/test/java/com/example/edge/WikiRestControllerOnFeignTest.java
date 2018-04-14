122cea3268b54345344c1b8735c9ac5b5569faa4
redutan <redutan@gmail.com>
2018-04-14 22:02:53 +0900

    Refectoring FeignTest

--- edge-service/src/test/java/com/example/edge/WikiRestControllerOnFeignTest.java
+++ edge-service/src/test/java/com/example/edge/WikiRestControllerOnFeignTest.java
@@ -1,32 +1,7 @@
 package com.example.edge;
 
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("feign")
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class WikiRestControllerOnFeignTest {
-    @Autowired
-    WebTestClient webTestClient;
-
-    @Test   // consul(설정도 같이) 서버와 wiki-service가 구동되어 있어야 한다.
-    public void topPages() {
-        webTestClient.get().uri("/wiki/pages")
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody()
-                .jsonPath("$").isArray();
-    }
+public class WikiRestControllerOnFeignTest extends WikiRestControllerTest {
 }
