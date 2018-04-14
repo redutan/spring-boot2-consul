53e4bc4cef8a8370e740d7f2e82c3dc09f4bc94d
redutan <redutan@gmail.com>
2018-04-14 23:39:07 +0900

    Apply Zuul proxy

--- edge-service/src/main/java/com/example/edge/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/example/edge/EdgeServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class EdgeServiceApplication {
 
     public static void main(String[] args) {
